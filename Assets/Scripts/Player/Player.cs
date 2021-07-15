a5b717b737197d2f5e0db60eab7f5fdcadea88db
KamelMahjoub <kamel.mahjoub@outlook.com>
2021-07-15 14:06:15 +0100

    Added Idle , Jump and run animations

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,73 +8,114 @@ public class Player : MonoBehaviour
 
     //Rigidbody2d reference
     private Rigidbody2D _rigid;
+
     //Variable for jumpforce
     [SerializeField]
-    private float _jumpForce = 5.0f;
-    //variable for grounded : if the char is on the ground or no
+    private float _jumpForce = 8.0f;
+
+    private bool _resetJump = false;
+
+   
     [SerializeField]
+    private float _speed = 3f;
+
+    //variable for grounded : if the char is on the ground or no
     private bool _grounded = false;
-    private bool resetJumpNeeded = false;
+
+    
+    private PlayerAnimation _playerAnim;
+
+    private SpriteRenderer _playerSprite;
+
+
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         _rigid = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
+        
     } 
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_grounded);
         Movement();
-        CheckGrounded();
     }
     
     void Movement()
     {
         //check for horizental input (left/right) 
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        //Current velocity = new velocity(HorizontalInput,Current velocity-y);
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        float move = Input.GetAxisRaw("Horizontal");
 
+       _grounded = IsGrounded();
+
+        Flip(move);
+       
+
+     
         //Jumping
         //if space key is pressed && grounded = true 
-        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+       if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
             //jump!
-            //current velocity = new velicy(Current x , Jumpforce) 
+            //current velocity = new velocity(Current x , Jumpforce) 
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
-            _grounded = false;
-            //jump cooldown 
-            resetJumpNeeded = true;
-            StartCoroutine(ResetJumpNeededRoutine());
+            StartCoroutine(ResetJumpRoutine());
+            _playerAnim.Jump(true);
         }
 
+        //Current velocity = new velocity(HorizontalInput,Current velocity-y);
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+
+        this._playerAnim.Move(move);
+
     }
-    void CheckGrounded()
+   
+   bool IsGrounded()
     {
-        //Raycasting to check if the player is on the ground or not , also sets the value of grounded to true or false
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 6);
-        //shows the ray/line to see if the ray is configured succesfully
+
         Debug.DrawRay(transform.position, Vector2.down * 1f, Color.green);
-        //if there's a collision , ray hits the ground
+
         if (hitInfo.collider != null)
         {
-            if (resetJumpNeeded == false)
-                _grounded = true;
+            if (_resetJump == false)
+            {
+                _playerAnim.Jump(false);
+                return true;
+            }
+
         }
+        return false;
     }
 
 
     //change the variable value after 0.1 seconds
-    IEnumerator ResetJumpNeededRoutine()
+    IEnumerator ResetJumpRoutine()
     {
+        _resetJump = true;
         yield return new WaitForSeconds(0.1f);
-        resetJumpNeeded = false;
+        _resetJump = false;
     }
 
 
+    void Flip(float move)
+    {
+        if (move > 0)
+        {
+            _playerSprite.flipX = false;
+            
 
+        }
+        else if (move < 0)
+        {
+            _playerSprite.flipX = true;
+
+        }
+    }
 }
 
---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator _anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jump(bool jumping)
+    {
+        _anim.SetBool("Jumping", jumping);
+    }
+
+}
