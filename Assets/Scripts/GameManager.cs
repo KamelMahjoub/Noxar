c08acc4f4fcf3abeb8ba721b08851326de965f0d
KamelMahjoub <kamel.mahjoub@outlook.com>
2021-07-25 19:23:28 +0100

    Added a game manager and a UI manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            //if its null , it'll break the game
+            if (_instance == null) ;
+            {
+                Debug.Log("Game Manager is Null!");
+            }
+            return _instance;
+        }
+    }
+    
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+}
